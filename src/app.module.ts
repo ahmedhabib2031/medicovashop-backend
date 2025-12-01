0ef54a2d06f512de98907156c026f35b1c8a08d7
Mo7hamed-el3adany <mohammedelsadany32@gmail.com>
2025-12-01 17:31:47 +0200

    Initial commit - NestJS e-commerce medicovashop backend

---
+++ src/app.module.ts
@@ -0,0 +1,51 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ShippingModule } from './shipping/shipping.module';
+import { CouponsModule } from './coupons/coupons.module';
+import { NotificationsModule } from './notifications/notifications.module';
+import { FaqModule } from './faq/faq.module';
+import { ReviewsModule } from './reviews/reviews.module';
+import { PaymentsModule } from './payments/payments.module';
+import { CartModule } from './cart/cart.module';
+import { OrdersModule } from './orders/orders.module';
+import { CategoriesModule } from './categories/categories.module';
+import { ProductsModule } from './products/products.module';
+import { AdminModule } from './admin/admin.module';
+import { SellersModule } from './sellers/sellers.module';
+import { TagsModule } from './tags/tags.module';
+import { AttributesModule } from './attributes/attributes.module';
+import { OptionsModule } from './options/options.module';
+import { SubCategoriesModule } from './sub-categories/sub-categories.module';
+import { DiscountModule } from './discount/discount.module';
+import { BrandsModule } from './brands/brands.module';
+import { TestimonialsModule } from './testimonials/testimonials.module';
+
+@Module({
+  imports: [
+    SellersModule,
+    AdminModule,
+    ProductsModule,
+    CategoriesModule,
+    OrdersModule,
+    CartModule,
+    PaymentsModule,
+    ReviewsModule,
+    FaqModule,
+    NotificationsModule,
+    CouponsModule,
+    ShippingModule,
+    UsersModule,
+    TagsModule,
+    AttributesModule,
+    OptionsModule,
+    SubCategoriesModule,
+    DiscountModule,
+    BrandsModule,
+    TestimonialsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
